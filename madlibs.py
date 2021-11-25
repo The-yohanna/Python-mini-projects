2b71688b2dfda23db38fa0755fc9d7e0ea99de06
Yohanna <Bradfordjohn998@gmail.com>
2021-11-25 11:41:50 +0300

    initial commit

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+
+adj = input("Adjective: ")
+adj2 = input("Adjective: ")
+lang1 = input("Programming language: ")
+lang2 = input("Programming language: ")
+verb = input("Verb: ")
+person = input("Person: ")
+
+
+madlibs = f"Computer programming is {adj}. There are many {adj2} languages. \
+Like {lang1} and {lang2}. All these make me {verb} and feel like a {person}"
+
+print(madlibs)
